7ebca051ce50e58757bed71fbbef2c095e0a9bfe
Ori Melamed <40656911+OriMelamed@users.noreply.github.com>
2023-06-08 10:46:44 +0300

    change title

--- src/components/ExerciseHeader.tsx
+++ src/components/ExerciseHeader.tsx
@@ -1,7 +1,7 @@
 function ExerciseHeader() {
   return (
     <div className=" flex justify-center text-4xl">
-      <h1 className=" border botder-black rounded-lg bg-pink-300 p-10 my-4">
+      <h1 className=" border botder-black rounded-lg bg-pink-300 p-10 m-4">
         חשבון של אנה
       </h1>
     </div>
